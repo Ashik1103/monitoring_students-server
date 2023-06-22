42d19d64d4b63bf2b5660c8cc23127145aca6779
Ashik <hajaashik11032002@gmail.com>
2023-06-22 15:38:53 +0530

    latest

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express= require('express');
+const express = require("express");
 const app = express();
-var admin=require("firebase-admin");
+var admin = require("firebase-admin");
 var serviceAccount = require("./config/push-notification-key.json");
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-app.use("/api",require("./route/app.route"));
-app.use("/data",require("./route/app.route"))
+app.use("/api", require("./route/app.route"));
+app.use("/data", require("./route/app.route"));
 var fcm = require("fcm-notification");
 const certPath = admin.credential.cert(serviceAccount);
 
@@ -19,142 +19,130 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var namee = "";
+var mentor = "";
 
-var namee=""
-
-
-app.get('/data', (req, res) => {
-    const db = admin.firestore();
-     console.log(req.body);
-      var wholeData = []
-      db.collection('students').get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          console.log(doc.id, '=>', doc.data());
-          // console.log(doc.data().name + doc.data().age);
-          // console.log(doc.data());
-          wholeData.push(doc.data())
-        });
-        console.log(wholeData)
-        res.send(wholeData)
-      })
-      .catch(error => {
-        console.log('Error!', error);
+app.get("/data", (req, res) => {
+  const db = admin.firestore();
+  console.log(req.body);
+  var wholeData = [];
+  db.collection("students")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        // console.log(doc.data().name + doc.data().age);
+        // console.log(doc.data());
+        wholeData.push(doc.data());
+      });
+      console.log(wholeData);
+      res.send(wholeData);
     })
-  })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+});
 
+app.post("/data", (req, res) => {
+  const db = admin.firestore();
 
+  let data = req.body;
+  console.log("printing the data");
+  console.log("inside the log");
+  console.log(JSON.stringify(data));
 
-  app.post('/data', (req, res) => {
-    const db = admin.firestore();
-     mentor=""
-     token=""
-    let data= req.body;
-    console.log("inside the log");
-    console.log(JSON.stringify(data));
-    usn= data.usn;
-    console.log(usn);
-      db.collection('students')
+  for (const key in data) {
+    console.log("printin the kwy:::::::::::");
+    console.log(key);
+    db.collection("students")
       .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-        //   console.log(doc.id, '=>', doc.data());
-          if (doc.id==usn){
-            console.log("Mentor is",doc.data().mentor);
-            mentor=doc.data().mentor;
-            namee=doc.data().name;
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          //   console.log(doc.id, '=>', doc.data());
+          if (doc.id == key) {
+            console.log("Mentor is", doc.data().mentor);
+            mentor = doc.data().mentor;
+            namee = doc.data().name;
           }
-        //   // console.log(doc.data().name + doc.data().age);
+          //   // console.log(doc.data().name + doc.data().age);
           // console.log(doc.data());
         });
         // console.log(wholeData)
         // res.send(wholeData)
       })
-      .catch(error => {
-        console.log('Error!', error);
-    })
+      .catch((error) => {
+        console.log("Error!", error);
+      });
 
-    db.collection('mentors')
+    db.collection("mentors")
       .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-            console.log("Inside mentors:::::::::::::::::::");
-          console.log(doc.id, '=>', doc.data());
-          if (doc.data().name==mentor){
-            console.log("Token is",doc.data().key);
-            token=doc.data().key;
-            console.log("printing the name::::::::::");
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          console.log("Inside mentors:::::::::::::::::::");
+          // console.log(doc.id, '=>', doc.data());
+          if (doc.data().name == mentor) {
+            console.log("Token is", doc.data().key);
+            token = doc.data().key;
+            console.log("logging the name::::::::::");
             console.log(namee);
-            sendNotification(token,namee)
+            sendNotification(token, namee);
+            addTODB(namee);
           }
-        //   // console.log(doc.data().name + doc.data().age);
+          //   // console.log(doc.data().name + doc.data().age);
           // console.log(doc.data());
         });
         // console.log(wholeData)
         // res.send(wholeData)
       })
-      .catch(error => {
-        console.log('Error!', error);
-    })
+      .catch((error) => {
+        console.log("Error!", error);
+      });
+  }
+});
 
+function addTODB(name) {
+  console.log("inside AddTODB");
+  const db = admin.firestore();
 
+  const d = new Date();
+  const da = d.toUTCString();
 
-
-    
-  })
-
-
-  
-
-
-
-function sendNotification(token,name){
-    try{
-        console.log("inside log");
-       
-      console.log(token,name)
-
-        let message = {
-            notification:{
-                title: "Bunk alert!!!!!!!!!!!" ,
-                body: name+" is not attending Class now"
-            },
-            data: {
-                orderId:"1234"
-            },
-            token:token
-
-        };
-        FCM.send(message,function(err,resp){
-            if(err){
-                console.log(err);
-            }
-            else{
-                console.log("Message sent:::::::::::::::::::::::::::::::::::::");
-            }
-        });
-    }
-    catch(err){
-        throw err;
-    }
+  db.collection("captured").add({
+    name: name,
+    date: da,
+  });
 }
 
+function sendNotification(token, name, date) {
+  try {
+    console.log("inside log");
 
+    console.log(token, name);
 
+    let message = {
+      notification: {
+        title: "Bunk alert!!!!!!!!!!!",
+        body: name + " is not attending Class now",
+      },
+      data: {
+        orderId: "1234",
+      },
+      token: token,
+    };
+    FCM.send(message, function (err, resp) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Message sent:::::::::::::::::::::::::::::::::::::");
+        // console.log(date);
+      }
+    });
+  } catch (err) {
+    throw err;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(4000,function(){
-    console.log("It's ready");
-})
+app.listen(4000, function () {
+  console.log("It's ready");
+});
 module.exports = app;
